efe77043c3ad7db8f2474653afb2c0091790d616
lufias69 <syaifulbachrimustamin@gmail.com>
2020-01-17 23:46:17 +0700

    Update update_rule.py

--- update_rule.py
+++ update_rule.py
@@ -4,7 +4,7 @@ import lib
 alamat = r"/etc/snort/rules/rules_custom"
 df = pd.read_csv("databaru.csv", sep=";")
 final = lib.find_used(df, batas_min = 10)
-rules = lib.get_rules(final, save = False)
+rules = lib.get_rules(final, save = True)
 print('pingattack', rules['pingattack'])
 print('sqlinjection', rules['sqlinjection'])
 print('synfloodattack', rules['synfloodattack'])
